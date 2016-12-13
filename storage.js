1a365537f671d567704452b0702d43f4e66e7b86
BrianGoyvaerts <r0595245@student.thomasmore.be>
2016-12-13 16:56:44 +0100

    Add files via upload

--- storage.js
+++ storage.js
@@ -1,7 +1,67 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var MongoClient = require('mongodb').MongoClient;
 
+var url = 'mongodb://localhost:27017/prober';
+// mongo in nodeJs: http://mongodb.github.io/node-mongodb-native/2.2/
+// mongo API : http://mongodb.github.io/node-mongodb-native/2.2/api/Collection.html
 
+var dal = {
+
+	connect: function (err, result) {
+		MongoClient.connect(url, function (error, db) {
+			if (error)
+				throw new Error(error);
+			console.log("Connected successfully to server");
+			result(db);
+		});
+	},
+	clearDrone: function (call) {
+		this.connect(null, function (db) {
+			db.collection('drones').drop(function (err, result) {
+				//callback(result);
+				db.close();
+			});
+		})
+	},
+	insertDrone: function (drone, callback) {
+		this.connect(null, function (db) {
+			db.collection('drones').insert(drone, function (err, result) {
+				//callback(result);
+				db.close();
+			});
+		});
+	},
+        clearFile: function (call) {
+		this.connect(null, function (db) {
+			db.collection('files').drop(function (err, result) {
+				//callback(result);
+				db.close();
+			});
+		})
+	},
+	insertFile: function (drone, callback) {
+		this.connect(null, function (db) {
+			db.collection('files').insert(files, function (err, result) {
+				//callback(result);
+				db.close();
+			});
+		});
+	},
+        clearContent: function (call) {
+		this.connect(null, function (db) {
+			db.collection('contents').drop(function (err, result) {
+				//callback(result);
+				db.close();
+			});
+		})
+	},
+	insertContent: function (contents, callback) {
+		this.connect(null, function (db) {
+			db.collection('contents').insert(contents, function (err, result) {
+				//callback(result);
+				db.close();
+			});
+		});
+	}
+};
+
+module.exports = dal;
